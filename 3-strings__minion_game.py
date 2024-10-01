9ec8c82c72f21256860b70f09edaace9c2605db3
deMartino1908303 <115620752+deMartino1908303@users.noreply.github.com>
2024-10-01 20:14:13 +0200

    Update 3-strings__minion_game.py

--- 3-strings__minion_game.py
+++ 3-strings__minion_game.py
@@ -1,9 +1,27 @@
-# Complete the solve function below.
-def solve(s):
-    lf=[]
-    l=s.split(" ")
-    for var in range(len(l)):
-        tmp = l[var].capitalize()
-        lf.append(tmp)
-    fin = ' '.join(lf)
-    return fin
+def minion_game(st):
+    # your code goes here
+    st_ln=len(st)
+    kv_scr=0
+    st_scr=0
+    
+    for val in range(st_ln):
+        
+        if str(st[val]) == 'A':
+            kv_scr += st_ln - val
+        elif str(st[val]) == 'E':
+            kv_scr += st_ln - val
+        elif str(st[val]) == 'I':
+            kv_scr += st_ln - val
+        elif str(st[val]) == 'O':
+            kv_scr += st_ln - val
+        elif str(st[val]) == 'U':
+            kv_scr += st_ln - val
+        else:
+            st_scr += st_ln -val
+            
+    if kv_scr > st_scr :
+        print ("Kevin", kv_scr)
+    elif kv_scr < st_scr:
+        print ("Stuart", st_scr)
+    else:
+        print ("Draw")
