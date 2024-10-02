25a3bb6512ff787344f13e59de787f7624c9aa25
deMartino1908303 <115620752+deMartino1908303@users.noreply.github.com>
2024-10-02 11:42:19 +0200

    Create 4-sets__check_subset.py

---
+++ 4-sets__check_subset.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+iterable = int(input())
+
+while  iterable > 0:
+    trash_1, set_1, trash_2, set_2 = input(), input().split(), input(), input().split()
+    set_1 = set(map(int, set_1))
+    set_2 = set(map(int, set_2))
+    if len(set_1.difference(set_2)) != 0: 
+        print(False)
+    else:
+        print(True)
