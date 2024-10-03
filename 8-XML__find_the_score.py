c817f21b876d87771d04ad78255df08f97c0dcaa
deMartino1908303 <115620752+deMartino1908303@users.noreply.github.com>
2024-10-03 16:49:49 +0200

    Update 8-XML__find_the_score.py

--- 8-XML__find_the_score.py
+++ 8-XML__find_the_score.py
@@ -9,3 +9,18 @@ def get_attr_number(node):
     return str(tree).count("=")
 
 if __name__ == '__main__':
+
+# i went back and did it in a more professional way :)
+
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(node):
+    # your code goes here
+    count = 0
+    for element in node.iter():
+        count += len(element.attrib)
+        
+    return count
+
+if __name__ == '__main__':
