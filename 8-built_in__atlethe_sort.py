d710282a4ee096b7f34ca00b176e4408611fcd40
deMartino1908303 <115620752+deMartino1908303@users.noreply.github.com>
2024-10-04 11:32:28 +0200

    Create 8-built_in__atlethe_sort.py

---
+++ 8-built_in__atlethe_sort.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+    
+    
+    for score in sorted(arr, key=lambda e: e[k]):
+        print(*score)
